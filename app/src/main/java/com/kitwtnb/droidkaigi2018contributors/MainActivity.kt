52238f3a7732e7ffdf90a0c38afb9daa82e2a39e
Keita Watanabe <Nal3eCh@gmail.com>
2018-02-12 17:38:24 +0900

    add menu items and events

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/MainActivity.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.kitwtnb.droidkaigi2018contributors.databinding.ActivityMainBinding
 import io.reactivex.rxkotlin.subscribeBy
@@ -31,5 +33,18 @@ class MainActivity : AppCompatActivity() {
             onSuccess = { Timber.i(it.toString()) },
             onError = { Timber.e(it) }
         )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item!!.itemId == R.id.oss_licenses) {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
