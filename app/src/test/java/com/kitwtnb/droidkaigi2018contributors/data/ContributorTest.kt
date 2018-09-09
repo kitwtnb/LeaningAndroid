590126dcc9457dd6923bd7876fbc75d20e180ce9
Keita Watanabe <watanabe.kit@gmail.com>
2018-09-09 21:03:14 +0900

    implement test

--- app/src/test/java/com/kitwtnb/droidkaigi2018contributors/data/ContributorTest.kt
+++ app/src/test/java/com/kitwtnb/droidkaigi2018contributors/data/ContributorTest.kt
@@ -4,6 +4,7 @@ import com.kitwtnb.droidkaigi2018contributors.di.jsonMapperModule
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
+import io.kotlintest.matchers.shouldBe
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -29,8 +30,29 @@ class ContributorTest : KoinTest {
     fun isCorrectConvertedFromJson() {
         val type = Types.newParameterizedType(List::class.java, Contributor::class.java)
         val jsonAdapter: JsonAdapter<List<Contributor>> = moshi.adapter(type)
-        jsonAdapter.fromJson(json).run {
-            // todo
+        jsonAdapter.fromJson(json)!!.run {
+            size shouldBe 1
+            first().run {
+                login shouldBe "kitwtnb"
+                id shouldBe 19305363
+                nodeId shouldBe "MDQ6VXNlcjE5MzA1MzYz"
+                avatarUrl shouldBe "https://avatars1.githubusercontent.com/u/19305363?v=4"
+                gravatarId shouldBe ""
+                url shouldBe "https://api.github.com/users/kitwtnb"
+                htmlUrl shouldBe "https://github.com/kitwtnb"
+                followersUrl shouldBe "https://api.github.com/users/kitwtnb/followers"
+                followingUrl shouldBe "https://api.github.com/users/kitwtnb/following{/other_user}"
+                gistsUrl shouldBe "https://api.github.com/users/kitwtnb/gists{/gist_id}"
+                starredUrl shouldBe "https://api.github.com/users/kitwtnb/starred{/owner}{/repo}"
+                subscriptionsUrl shouldBe "https://api.github.com/users/kitwtnb/subscriptions"
+                organizationsUrl shouldBe "https://api.github.com/users/kitwtnb/orgs"
+                reposUrl shouldBe "https://api.github.com/users/kitwtnb/repos"
+                eventsUrl shouldBe "https://api.github.com/users/kitwtnb/events{/privacy}"
+                receivedEventsUrl shouldBe "https://api.github.com/users/kitwtnb/received_events"
+                this.type shouldBe "User"
+                siteAdmin shouldBe false
+                contributions shouldBe 61
+            }
         }
     }
 }
