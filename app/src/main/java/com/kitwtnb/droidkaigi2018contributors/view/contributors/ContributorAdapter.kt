aa33f03cd64a9c10091a7482dd6c608f0870d60d
Keita Watanabe <Nal3eCh@gmail.com>
2018-09-13 16:00:47 +0900

    implement contributor view

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/view/contributors/ContributorAdapter.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/view/contributors/ContributorAdapter.kt
@@ -1,20 +1,27 @@
 package com.kitwtnb.droidkaigi2018contributors.view.contributors
 
-import android.app.Activity
-import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.kitwtnb.droidkaigi2018contributors.R
 import com.kitwtnb.droidkaigi2018contributors.databinding.ViewContributorItemBinding
 import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
 
-class ContributorAdapter(private val contributors: List<Contributor>) : RecyclerView.Adapter<ContributorAdapter.BindingHolder>() {
+class ContributorAdapter : RecyclerView.Adapter<ContributorAdapter.BindingHolder>() {
     class BindingHolder(val binding: ViewContributorItemBinding) : RecyclerView.ViewHolder(binding.root)
 
+    private val contributors = mutableListOf<Contributor>()
+
+    override fun getItemCount() = contributors.size
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
-        val binding = DataBindingUtil.setContentView<ViewContributorItemBinding>(parent.context as Activity, R.layout.view_contributor_item).also {
+        val activity = parent.context as AppCompatActivity
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ViewContributorItemBinding.inflate(inflater, parent, false).also {
+            it.setLifecycleOwner(activity)
             it.viewModel = ContributorItemViewModel()
         }
+
         return BindingHolder(binding)
     }
 
@@ -22,5 +29,8 @@ class ContributorAdapter(private val contributors: List<Contributor>) : Recycler
         holder.binding.viewModel!!.setContributor(contributors[position])
     }
 
-    override fun getItemCount() = contributors.size
+    fun setContributors(contributors: List<Contributor>) {
+        this.contributors.addAll(contributors)
+        notifyDataSetChanged()
+    }
 }
