b31d7a552148e2ca6323828fd6f81a5d9565d905
Keita Watanabe <watanabe.kit@gmail.com>
2018-09-20 10:26:07 +0900

    add db module

---
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/datastore/db/ContributorDao.kt
@@ -0,0 +1,23 @@
+package com.kitwtnb.droidkaigi2018contributors.datastore.db
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
+import kotlinx.coroutines.experimental.async
+
+@Dao
+interface ContributorDao {
+    @Query("SELECT * FROM contributor")
+    fun fetch(): List<Contributor>
+
+    @Insert
+    fun insert(contributors: List<Contributor>)
+
+    @Query("DELETE FROM contributor")
+    fun delete()
+}
+
+fun ContributorDao.deferredFetch() = async { fetch() }
+fun ContributorDao.deferredInsert(contributors: List<Contributor>) = async { insert(contributors) }
+fun ContributorDao.deferredDelete() = async { delete() }
