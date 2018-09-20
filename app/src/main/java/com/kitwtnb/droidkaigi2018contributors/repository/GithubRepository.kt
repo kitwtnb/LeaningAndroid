267a161566eccb4c5580fec958e1aaa088728213
Keita Watanabe <Nal3eCh@gmail.com>
2018-09-20 11:54:32 +0900

    use caching

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/repository/GithubRepository.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/repository/GithubRepository.kt
@@ -1,13 +1,30 @@
 package com.kitwtnb.droidkaigi2018contributors.repository
 
 import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
+import com.kitwtnb.droidkaigi2018contributors.datastore.db.ContributorDao
+import com.kitwtnb.droidkaigi2018contributors.datastore.db.deferredFetch
+import com.kitwtnb.droidkaigi2018contributors.datastore.db.deferredInsert
 import com.kitwtnb.droidkaigi2018contributors.datastore.service.GithubService
 import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
 
 interface GithubRepository {
-    fun loadContributors(owner: String, repository: String): Deferred<List<Contributor>>
+    suspend fun loadContributors(owner: String, repository: String): Deferred<List<Contributor>>
 }
 
-class GithubRepositoryImpl(private val service: GithubService) : GithubRepository {
-    override fun loadContributors(owner: String, repository: String) = service.contributors(owner, repository)
+class GithubRepositoryImpl(
+        private val service: GithubService,
+        private val contributorDao: ContributorDao
+) : GithubRepository {
+    override suspend fun loadContributors(owner: String, repository: String): Deferred<List<Contributor>> {
+        val cache = contributorDao.deferredFetch().await()
+        if (cache.isNotEmpty()) {
+            return async { cache }
+        }
+
+        val contributors = service.contributors(owner, repository).await()
+        contributorDao.deferredInsert(contributors).await()
+
+        return async { contributors }
+    }
 }
