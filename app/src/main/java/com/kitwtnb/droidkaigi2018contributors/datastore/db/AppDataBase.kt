02b8b4201c7c4fab59bef894d5f074779f1fc4d1
Keita Watanabe <Nal3eCh@gmail.com>
2018-09-21 14:52:35 +0900

    change entity

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/datastore/db/AppDataBase.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/datastore/db/AppDataBase.kt
@@ -2,11 +2,11 @@ package com.kitwtnb.droidkaigi2018contributors.datastore.db
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
+import com.kitwtnb.droidkaigi2018contributors.datastore.data.ContributorEntity
 
 @Database(
         entities = [
-            Contributor::class
+            ContributorEntity::class
         ],
         version = 1
 )
--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/datastore/db/ContributorDao.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/datastore/db/ContributorDao.kt
@@ -5,27 +5,32 @@ import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
 import android.arch.persistence.room.Transaction
 import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
+import com.kitwtnb.droidkaigi2018contributors.datastore.data.ContributorEntity
 import kotlinx.coroutines.experimental.async
 
 @Dao
 interface ContributorDao {
-    @Query("SELECT * FROM contributor ORDER BY contributions DESC")
-    fun fetch(): List<Contributor>
+    @Query("SELECT * FROM contributor where owner = :owner and repository = :repository ORDER BY contributions DESC")
+    fun fetchBy(owner: String, repository: String): List<ContributorEntity>
 
     @Insert
-    fun insert(contributors: List<Contributor>)
+    fun insert(contributors: List<ContributorEntity>)
 
     @Query("DELETE FROM contributor")
     fun delete()
 
+    @Query("DELETE FROM contributor where owner = :owner and repository = :repository")
+    fun deleteBy(owner: String, repository: String)
+
     @Transaction
-    fun replace(contributors: List<Contributor>) {
-        delete()
-        insert(contributors)
+    fun replaceBy(owner: String, repository: String, contributors: List<Contributor>) {
+        deleteBy(owner, repository)
+        insert(contributors.map { ContributorEntity(owner, repository, it) })
     }
 }
 
-fun ContributorDao.deferredFetch() = async { fetch() }
-fun ContributorDao.deferredInsert(contributors: List<Contributor>) = async { insert(contributors) }
+fun ContributorDao.deferredFetchBy(owner: String, repository: String) = async { fetchBy(owner, repository) }
+fun ContributorDao.deferredInsert(contributors: List<ContributorEntity>) = async { insert(contributors) }
 fun ContributorDao.deferredDelete() = async { delete() }
-fun ContributorDao.deferredReplace(contributors: List<Contributor>) = async { replace(contributors) }
+fun ContributorDao.deferredDeleteBy(owner: String, repository: String) = async { deleteBy(owner, repository) }
+fun ContributorDao.deferredReplace(owner: String, repository: String, contributors: List<Contributor>) = async { replaceBy(owner, repository, contributors) }
