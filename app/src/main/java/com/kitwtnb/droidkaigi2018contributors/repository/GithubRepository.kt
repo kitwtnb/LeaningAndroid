cd7f7a912e41dfc131db1714bf062e329dee836c
Keita Watanabe <watanabe.kit@gmail.com>
2018-09-21 10:52:43 +0900

    delete cache when launch application

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/repository/GithubRepository.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/repository/GithubRepository.kt
@@ -2,6 +2,7 @@ package com.kitwtnb.droidkaigi2018contributors.repository
 
 import com.kitwtnb.droidkaigi2018contributors.datastore.data.Contributor
 import com.kitwtnb.droidkaigi2018contributors.datastore.db.ContributorDao
+import com.kitwtnb.droidkaigi2018contributors.datastore.db.deferredDelete
 import com.kitwtnb.droidkaigi2018contributors.datastore.db.deferredFetch
 import com.kitwtnb.droidkaigi2018contributors.datastore.db.deferredInsert
 import com.kitwtnb.droidkaigi2018contributors.datastore.service.GithubService
@@ -10,6 +11,7 @@ import kotlinx.coroutines.experimental.async
 
 interface GithubRepository {
     suspend fun loadContributors(owner: String, repository: String): Deferred<List<Contributor>>
+    suspend fun deleteContributors(): Deferred<Unit>
 }
 
 class GithubRepositoryImpl(
@@ -27,4 +29,6 @@ class GithubRepositoryImpl(
 
         return async { contributors }
     }
+
+    override suspend fun deleteContributors(): Deferred<Unit> = contributorDao.deferredDelete()
 }
