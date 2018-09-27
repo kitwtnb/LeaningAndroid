a3f57fb9e98d2b876252d1926738ef54b882ce91
Keita Watanabe <Nal3eCh@gmail.com>
2018-09-27 10:12:09 +0900

    use new method

--- app/src/main/java/com/kitwtnb/droidkaigi2018contributors/di/ViewModelModule.kt
+++ app/src/main/java/com/kitwtnb/droidkaigi2018contributors/di/ViewModelModule.kt
@@ -3,10 +3,12 @@ package com.kitwtnb.droidkaigi2018contributors.di
 import com.kitwtnb.droidkaigi2018contributors.view.contributors.ContributorsViewModel
 import com.kitwtnb.droidkaigi2018contributors.view.main.MainViewModel
 import kotlinx.coroutines.experimental.android.UI
-import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.android.viewmodel.experimental.builder.viewModel
 import org.koin.dsl.module.module
+import kotlin.coroutines.experimental.CoroutineContext
 
 val viewModelModule = module {
-    viewModel { MainViewModel(get(), get()) }
-    viewModel { ContributorsViewModel(UI, get()) }
+    single { UI as CoroutineContext }
+    viewModel<MainViewModel>()
+    viewModel<ContributorsViewModel>()
 }
