0a4e36fd82a7f1ab00e70876df9eba36fb9adc86
Keita Watanabe <watanabe.kit@gmail.com>
2020-06-29 14:14:44 +0900

    implement transition

--- app/src/main/java/com/github/kitwtnb/learningandroid/MainActivity.kt
+++ app/src/main/java/com/github/kitwtnb/learningandroid/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.github.kitwtnb.learningandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kitwtnb.learningandroid.fluxsample.FluxSampleActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        go_to_flux_sample_button.setOnClickListener {
+            val intent = Intent(this, FluxSampleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ fluxsample/src/main/java/com/github/kitwtnb/learningandroid/fluxsample/FluxSampleActivity.kt
@@ -0,0 +1,11 @@
+package com.github.kitwtnb.learningandroid.fluxsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FluxSampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flux_sample)
+    }
+}
